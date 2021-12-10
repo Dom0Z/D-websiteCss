f72c4323f0d597344a9fea8a0a38d4f00f7bfa9f
Dom0Z <dzilenas@hawk.iit.edu>
2021-12-09 18:09:56 -0600

    Added dark mode functionality along with and image input button that changes when clicked

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
-function nightMode(){
+function nightMode(input){
   var element = document.body;
   element.classList.toggle("lightmode");
+  console.log(input.getAttribute('src'));
+
+
+  if(input.getAttribute('src') == "../Icons/sunpixel.png"){
+    input.src = "../Icons/moonpixel.png";
+  } else{
+    input.src ="../Icons/sunpixel.png";
+  }
+  
+  
 }
